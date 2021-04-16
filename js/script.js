19cf72847b265175632153fad9352012ad501199
rhenry <ramoneh94@gmail.com>
2021-04-16 15:01:44 -0400

    fixed navigation

--- js/script.js
+++ js/script.js
@@ -147,6 +147,11 @@ const heading = document.querySelector(".heading");
 heading.addEventListener("mousemove", onMouseMove);
 heading.addEventListener("touchmove", onTouchMove);
 
+// const projects = document.querySelector(".projects");
+// projects.addEventListener("mousemove", onMouseMove);
+// heading.addEventListener("touchmove", onTouchMove);
+
+
 
 /* 
 fade in content effect 
@@ -177,26 +182,3 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hideme_2').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade in */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1000);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
